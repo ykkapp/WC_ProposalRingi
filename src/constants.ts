54426ab24f1b2a00ff816dac1770504b16ed76a7
YutoKagami <kagamigami1210@gmail.com>
2026-03-09 13:15:55 +0900

    URL再更新

--- src/constants.ts
+++ src/constants.ts
@@ -449,7 +449,7 @@ export const FAQ_DATA: FaqItem[] = [
             {
               title: "事業部変更を伴う",
               link: "【WC-G】原価変更_給与改定無",
-              url: "https://sites.google.com/nareru-group.co.jp/ringi-37/%E3%83%9B%E3%83%BC%E3%83%A0",
+              url: "https://sites.google.com/nareru-group.co.jp/ringi-27/%E3%83%9B%E3%83%BC%E3%83%A0",
               details: {
                 applicableProject:
                   "所属事業部変更に関する事項（事業部の変更に限定される場合)",
@@ -517,7 +517,7 @@ export const FAQ_DATA: FaqItem[] = [
             {
               title: "事業部変更を伴う",
               link: "【WC-G】原価変更_給与改定有",
-              url: "https://sites.google.com/nareru-group.co.jp/ringi-36/%E3%83%9B%E3%83%BC%E3%83%A0",
+              url: "https://sites.google.com/nareru-group.co.jp/ringi-26/%E3%83%9B%E3%83%BC%E3%83%A0",
               details: {
                 applicableProject:
                   "所属事業部変更に関する事項（給与・雇用形態変更が伴う場合）",
@@ -559,7 +559,7 @@ export const FAQ_DATA: FaqItem[] = [
             {
               title: "単純な事業部変更",
               link: "【WC-G】原価変更_給与改定無",
-              url: "https://sites.google.com/nareru-group.co.jp/ringi-37/%E3%83%9B%E3%83%BC%E3%83%A0",
+              url: "https://sites.google.com/nareru-group.co.jp/ringi-27/%E3%83%9B%E3%83%BC%E3%83%A0",
               details: {
                 applicableProject:
                   "所属事業部変更に関する事項（事業部の変更に限定される場合)",
